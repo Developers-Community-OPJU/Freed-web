608eaf4480a8d4b24b05868721b156b8c3f2813f
Roshan nahak <roshannahak1122@gmail.com>
2021-08-02 10:18:12 +0530

    change theme color

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f81169526ec68bccb240fc78fdadd7de",
-"/": "f81169526ec68bccb240fc78fdadd7de",
-"main.dart.js": "a7a897bda875b2d341b6f84ee0384bc1",
+"index.html": "2b85ad8c78059503770cba7b433791fe",
+"/": "2b85ad8c78059503770cba7b433791fe",
+"main.dart.js": "f0babcd442d60c0ef7b49c83fb01a676",
 "manifest.json": "55e2ea5cdb6b4a1878f71ee3da1c2583",
 "version.json": "a40c0dac21dccdf938812414dc66509c"
 };
